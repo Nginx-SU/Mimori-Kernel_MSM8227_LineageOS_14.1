783b64b2db9c5c6fc8c0313e1f20ebcd2d1a021c
Daniel Rosenberg <drosen@google.com>
2016-03-23 12:09:25 -0700

    Fix kernel compile error

--- drivers/usb/gadget/android.c
+++ drivers/usb/gadget/android.c
@@ -2090,6 +2090,42 @@ static struct android_usb_function midi_function = {
 	.attributes	= midi_function_attributes,
 };
 
+static int hid_function_init(struct android_usb_function *f, struct usb_composite_dev *cdev)
+{
+ 	return ghid_setup(cdev->gadget, 2);
+}
+ 
+static void hid_function_cleanup(struct android_usb_function *f)
+{
+ 	ghid_cleanup();
+}
+ 
+static int hid_function_bind_config(struct android_usb_function *f, struct usb_configuration *c)
+{
+ 	int ret;
+ 	printk(KERN_INFO "hid keyboard\n");
+ 	ret = hidg_bind_config(c, &ghid_device_android_keyboard, 0);
+ 	if (ret) {
+ 		pr_info("%s: hid_function_bind_config keyboard failed: %d\n", __func__, ret);
+ 		return ret;
+ 	}
+ 	printk(KERN_INFO "hid mouse\n");
+ 	ret = hidg_bind_config(c, &ghid_device_android_mouse, 1);
+ 	if (ret) {
+ 		pr_info("%s: hid_function_bind_config mouse failed: %d\n", __func__, ret);
+ 		return ret;
+ 	}
+ 	return 0;
+}
+ 
+static struct android_usb_function hid_function = {
+ 	.name		= "hid",
+ 	.init		= hid_function_init,
+ 	.cleanup	= hid_function_cleanup,
+ 	.bind_config	= hid_function_bind_config,
+
+};
+
 static struct android_usb_function *supported_functions[] = {
 	&mbim_function,
 	&ecm_qc_function,
