cf30ad2e717a41643d4ac23718f5fb8562143bfe
Dicky Herlambang <herlambangdicky5@gmail.com>
2017-08-16 16:53:12 +0700

    Mimori kernel first build

--- Mimori.sh
+++ Mimori.sh
@@ -17,8 +17,8 @@ export ARCH=arm
 export CROSS_COMPILE=/home/Hana/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-
 
 echo "
-###Building Hana Kernel"
-make ARCH=arm mimori_kernel_nicki_defconfig
+###Building Mimori Kernel"
+make ARCH=arm mimori_nicki_defconfig
 make ARCH=arm CROSS_COMPILE=/home/Hana/arm-linux-androideabi-4.9/bin/arm-linux-androideabi- > mimori.log
 
 echo "
@@ -27,8 +27,8 @@ mkdir modules
 cp arch/arm/boot/zImage modules
 find . -name "*.ko" -exec cp {} modules \;
 cd modules
-/home/Hana/mimori/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-strip --strip-unneeded *.ko
-cd /home/Hana/android_kernel_sony_msm8x27-cm-14.1
+/home/Hana/arm-linux-androideabi-4.9/bin/arm-linux-androideabi-strip --strip-unneeded *.ko
+cd /home/Hana/Mimori-Kernel
 mv modules TEMP
 
 echo "
